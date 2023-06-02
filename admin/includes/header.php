91fadfa798ebd9b79f5d42a82ae4cd635f766da9
Saad Mehmood <71284963+saadMehmood123@users.noreply.github.com>
2023-06-02 19:57:41 +0500

    CSMS

---
+++ admin/includes/header.php
@@ -0,0 +1,14 @@
+<div class="brand clearfix" style="background-color: #6f6f6f; position:fixed; top:0;">
+	<a href="dashboard.php" style="font-size: 20px; color:white; line-height:60px;padding-left:20px;">CSMS Portal | Admin Panel</a>
+	<span class="menu-btn"><i class="fa fa-bars"></i></span>
+	<ul class="ts-profile-nav">
+
+		<li class="ts-account">
+			<a href="#"><img src="img/ts-avatar.png" class="ts-avatar hidden-side" alt=""> Account <i class="fa fa-angle-down hidden-side"></i></a>
+			<ul>
+				<li><a href="change-password.php">Change Password</a></li>
+				<li><a href="logout.php">Logout</a></li>
+			</ul>
+		</li>
+	</ul>
+</div>
---
+++ admin/includes/leftbar.php
@@ -0,0 +1,31 @@
+	<nav class="ts-sidebar" style="background:#6f6d6d;color:#ffffff;">
+		<ul class="ts-sidebar-menu" style="color:#fff;">
+
+			<li class="ts-label" style="color:#fff;">Main</li>
+			<li><a href="dashboard.php"><i class="fa fa-dashboard" style="color: #fff;"></i> Dashboard</a></li>
+
+			<li><a href="#"><i class="fa fa-files-o" style="color: #fff;"></i> Brands</a>
+				<ul>
+					<li><a href="create-brand.php">Create Brand</a></li>
+					<li><a href="manage-brands.php">Manage Brands</a></li>
+				</ul>
+			</li>
+
+			<li><a href="#"><i class="fa fa-sitemap" style="color: #fff;"></i> Vehicles</a>
+				<ul>
+					<li><a href="post-avehical.php">Post a Vehicle</a></li>
+					<li><a href="manage-vehicles.php">Manage Vehicles</a></li>
+				</ul>
+			</li>
+			<li><a href="manage-bookings.php"><i class="fa fa-users" style="color: #fff;"></i> Manage Buying Request</a></li>
+
+			<li><a href="testimonials.php"><i class="fa fa-table" style="color: #fff;"></i> Manage Testimonials</a></li>
+			<li><a href="manage-conactusquery.php"><i class="fa fa-desktop" style="color: #fff;"></i> Manage Contact us query</a></li>
+			<li><a href=" reg-users.php"><i class="fa fa-users" style="color: #fff;"></i> Registered Users</a></li>
+			<li><a href="manage-pages.php"><i class="fa fa-files-o" style="color: #fff;"></i> Manage Pages</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o" style="color: #fff;"></i> Update Contact Info</a></li>
+
+			<li><a href="manage-subscribers.php"><i class="fa fa-table" style="color: #fff;"></i> Manage Subscribers</a></li>
+
+		</ul>
+	</nav>
